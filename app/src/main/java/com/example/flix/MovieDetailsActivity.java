331f19b40d3e9172838e0a43e36a304807cc45a0
Kanika Sharma <kanika22@fb.com>
2020-06-26 12:23:00 -0400

    Fixed up view

--- app/src/main/java/com/example/flix/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flix/MovieDetailsActivity.java
@@ -2,11 +2,13 @@ package com.example.flix;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.flix.models.Movie;
 
 import org.parceler.Parcels;
@@ -22,6 +24,8 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvOverview;
     RatingBar rbVoteAverage;
     TextView tvreleaseDate;
+    ImageView image_view;
+    String url;
 
     @Override
     protected void onCreate( Bundle savedInstanceState){
@@ -32,12 +36,14 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
         tvreleaseDate = (TextView) findViewById(R.id.tvreleaseDate);
+        image_view = (ImageView) findViewById(R.id.imageView);
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s", movie.getTitle()));
 
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
         tvreleaseDate.setText(movie.getReleaseDate());
+        Glide.with(this).load(movie.getPosterPath()).override(600, 1200).into(image_view);
         float voteAvg = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAvg = voteAvg > 0 ? voteAvg / 2.0f : voteAvg);
     }
